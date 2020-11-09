19b81ab369735b4313cec641a552be40b07304d5
c0d3in3 <tedex.manvelidze@gmail.com>
2020-11-09 11:52:00 +0400

    Revert "Handle network connectivity"
    This reverts commit 1369cc249b2ae7a537853548a113f629de3f36c3.

--- app/src/main/java/com/c0d3in3/movieapp/App.kt
+++ app/src/main/java/com/c0d3in3/movieapp/App.kt
@@ -2,9 +2,7 @@ package com.c0d3in3.movieapp
 
 import android.app.Application
 import android.content.Context
-import androidx.room.Room
 import com.c0d3in3.movieapp.data.local.AppDatabase
-import com.c0d3in3.movieapp.utils.Constants
 
 class App : Application() {
 
